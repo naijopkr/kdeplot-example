c1d1979cecd36e113c60518d8b1765a4f817bd5e
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-04-14 15:47:47 -0500

    plots to graphs

--- __main__.py
+++ __main__.py
@@ -5,23 +5,18 @@ from scipy import stats
 
 def main():
     dataset = np.random.randn(25)
-    print(dataset)
 
+    plt.subplot(2,1,1)
     sns.rugplot(dataset)
 
     x_min = dataset.min() - 2
     x_max = dataset.max() + 2
-    print(x_min)
-    print(x_max)
 
     x_axis = np.linspace(x_min, x_max, 100)
-    print(x_axis)
 
     bandwidth = ((4 * dataset.std()**5) / (3 * len(dataset))) ** 0.2
-    print(bandwidth)
 
     kernel_list = []
-
     for data_point in dataset:
         kernel = stats.norm(data_point, bandwidth).pdf(x_axis)
         kernel_list.append(kernel)
@@ -31,7 +26,9 @@ def main():
         plt.plot(x_axis, kernel, color='grey', alpha=0.5)
 
     sum_of_kde = np.sum(kernel_list, axis=0)
-    fig = plt.plot(x_axis, sum_of_kde, color='indianred')
+
+    plt.subplot(2,1,2)
+    plt.plot(x_axis, sum_of_kde, color='indianred')
 
     plt.savefig('output.pdf')
 
